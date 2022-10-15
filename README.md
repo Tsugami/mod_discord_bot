1ad309971f645046e6000c05243cfefed29094dd
Yslan Ramos <yslanramos.r@gmail.com>
2022-10-15 02:12:44 -0300

    feat: add migrations

---
+++ README.md
@@ -0,0 +1,9 @@
+# Mod Discord Bot
+
+adw
+
+## Como entrar no container do PosgreSQL
+
+`docker exec -it <container_name> bash`
+
+`psql -d mydb -U johndoe`
---
+++ migrations/init.sql
@@ -0,0 +1,14 @@
+CREATE TYPE
+    voice_state_update_type AS ENUM ('JOIN', 'LEAVE', 'SWITCH');
+
+CREATE TABLE
+    "voice_state_update" (
+        id SERIAL PRIMARY KEY,
+        channel_id TEXT NOT NULL,
+        guild_id TEXT NOT NULL,
+        user_id TEXT NOT NULL,
+        type voice_state_update_type NOT NULL,
+        created_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP
+    );
+
+CREATE UNIQUE INDEX "voice_state_update_user_idx" ON "voice_state_update" ("user_id")
